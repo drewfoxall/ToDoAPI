9ca16da053bf36017518f079870a54c5a1828b18
drewfoxall <andrewfoxall2001@gmail.com>
2026-05-22 21:55:53 +0800

    feat: Middleware will not use handler, authorization working using JWT_SECRET token password

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,15 +3,20 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/xiaoan1122/todo-api/internal/handlers"
+	"github.com/xiaoan1122/todo-api/internal/middleware"
 	"gorm.io/gorm"
 )
 
 func RegisterTodoRoutes(router *gin.Engine, db *gorm.DB) {
+
 	handler := handlers.NewTodoHandler(db)
 
-	router.GET("/todos", handler.GetTodos)
-	router.GET("/todos/:id", handler.GetTodoByID)
-	router.POST("/todos", handler.CreateTodo)
-	router.PUT("/todos/:id", handler.UpdateTodo)
-	router.DELETE("/todos/:id", handler.DeleteTodo)
+	authorized := router.Group("/")
+	authorized.Use(middleware.AuthMiddleware())
+
+	authorized.GET("/todos", handler.GetTodos)
+	authorized.GET("/todos/:id", handler.GetTodoByID)
+	authorized.POST("/todos", handler.CreateTodo)
+	authorized.PUT("/todos/:id", handler.UpdateTodo)
+	authorized.DELETE("/todos/:id", handler.DeleteTodo)
 }
